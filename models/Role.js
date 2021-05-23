7d259246ef02cc439f4607be3431d3b99038d650
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-05-23 15:38:02 -0400

    role.js

---
+++ models/Role.js
@@ -0,0 +1,38 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../db/connection');
+
+class Role extends Model{}
+
+Role.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataType.STRING,
+        },
+        salary: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        department_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'department',
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'role',
+    }
+);
+module.exports = Role;
